eb20dbb00ddf2575694bae09004497e69cfbd24b
Abhilash Vangari <100797186+av1289@users.noreply.github.com>
2023-03-30 02:11:12 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,5 +2,10 @@ module "network" {
   source  = "app.terraform.io/sruthi/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "sruthi"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+ resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
 }
